7038e740d7d05eb4623fc4757160b27329ca94ed
E-milia <es.ponomareva@gmail.com>
2017-07-17 17:59:29 +0100

    add simple user query

--- test-data/user.js
+++ test-data/user.js
@@ -5,22 +5,34 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 const usersRef = database.ref('/users');
+const users = require('./seed/users.json')
 
-const email = 'John.Smith3@gmail.com';
 const password = 'password123'
+const email = 'John.Jones@google.com'
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
+
+const keys = Object.keys(users);
+
+const promises = keys.map(key => {
+    firebase.auth().createUserWithEmailAndPassword(`${key}@gmail.com`, password)
+});
+
+Promise.all(promises)
     .then(function (data) {
-        usersRef.push({
-            ID: data.uid,
-            firstName: 'John',
-            lastName: 'Smith',
-            password: password,
-            phoneNumber: 0161123456,
-            createdOn: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
-            avatar: 'www.google.com/avatar.jpg'
-        });
+        console.log(data)
     })
+    // firebase.auth().createUserWithEmailAndPassword(email, password)
+    //     .then(function (data) {
+    //         usersRef.push({
+    //             ID: data.uid,
+    //             firstName: 'John',
+    //             lastName: 'Smith',
+    //             password: password,
+    //             phoneNumber: 0161123456,
+    //             createdOn: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
+    //             avatar: 'www.google.com/avatar.jpg'
+    //         });
+    //     })
     .catch(function (err) {
         console.log(err)
     });
