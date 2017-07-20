b982822ef3a30d7102f0a76f3e2e7f593d5bc72c
E-milia <es.ponomareva@gmail.com>
2017-07-20 16:11:56 +0100

    refactor add note function

--- test-data/note.js
+++ test-data/note.js
@@ -4,14 +4,6 @@ const database = require('../config');
 const notes = require('./seed/notes.json')
 const keys = Object.keys(notes);
 
-// const note = {
-//     created: Date.now(),
-//     title: 'The Brothers Karamazov',
-//     text: 'Doomed',
-//     tags: ['code', 'firebase'],
-//     lastEditTime: Date.now()
-// };
-
 firebase.auth().signInWithEmailAndPassword('John.Smith@google1.com', 'password123')
     .then((data) => {
         return data.uid;
@@ -31,3 +23,25 @@ firebase.auth().signInWithEmailAndPassword('John.Smith@google1.com', 'password12
     .catch(function (err) {
         console.log(err);
     });
+
+
+export default addNote(e) {
+    e.preventDefault();
+    const note = {
+        created: Date.now(),
+        title: 'The Brothers Karamazov',
+        text: 'Doomed',
+        tags: ['code', 'firebase'],
+        lastEditTime: Date.now()
+    };
+    const userId = firebase.auth().currentUser.uid;
+    const notesRef = database.ref(`/notes/${userId}`);
+    return notesRef.push(note)
+        .then(res => {
+            note.key = res.key;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+}
