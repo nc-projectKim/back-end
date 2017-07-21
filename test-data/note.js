3a3e351f250bb73e638e7b7943f7ea7dd6073bd2
E-milia <es.ponomareva@gmail.com>
2017-07-21 14:54:35 +0100

    minor changes to exportable functions

--- test-data/note.js
+++ test-data/note.js
@@ -4,57 +4,20 @@ const database = require('../config');
 const notes = require('./seed/notes.json')
 const keys = Object.keys(notes);
 
-    const note = {
-        created: 'banana',
-        text: 'Doomed',
-        tags: ['code', 'firebase'],
-        lastEditTime: null
-    }
-
-firebase.auth().signInWithEmailAndPassword('John.Smith@google1.com', 'password123')
-    .then((data) => {
-        return data.uid;
-    })
-    .then((id) => {
-        const notesRef = database.ref(`/notes/${id}`);
-            return notesRef.push(note)
-        .then(res => {
-            note.key = res.key;
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-    });
-
-
-    //     const promises = keys.map(key => {
-    //         return notesRef.push(notes[key])
-    //     })
-    //     return Promise.all(promises)
-    // })
-    // .then(function (data) {
-    //     console.log(data)
-    //     // notesRef.on('child_added', (snap) => console.log(snap.val())) 
-    //     // return notesRef.push(note);
-    // })
-    // .catch(function (err) {
-    //     console.log(err);
-    // });
-
-
-// export default addNote(e) {
-//     e.preventDefault();
 //     const note = {
-//         created: Date.now(),
-//         title: 'The Brothers Karamazov',
+//         created: 'banana',
 //         text: 'Doomed',
 //         tags: ['code', 'firebase'],
-//         lastEditTime: Date.now()
-//     };
-//     const userId = firebase.auth().currentUser.uid;
-//     const notesRef = database.ref(`/notes/${userId}`);
-//     return notesRef.push(note)
+//         lastEditTime: null
+//     }
+
+// firebase.auth().signInWithEmailAndPassword('John.Smith@google1.com', 'password123')
+//     .then((data) => {
+//         return data.uid;
+//     })
+//     .then((id) => {
+//         const notesRef = database.ref(`/notes/${id}`);
+//             return notesRef.push(note)
 //         .then(res => {
 //             note.key = res.key;
 //         })
@@ -62,4 +25,47 @@ firebase.auth().signInWithEmailAndPassword('John.Smith@google1.com', 'password12
 //             console.log(err);
 //         });
 
-// }
+//     });
+
+
+//     const promises = keys.map(key => {
+//         return notesRef.push(notes[key])
+//     })
+//     return Promise.all(promises)
+// })
+// .then(function (data) {
+//     console.log(data)
+//     // notesRef.on('child_added', (snap) => console.log(snap.val())) 
+//     // return notesRef.push(note);
+// })
+// .catch(function (err) {
+//     console.log(err);
+// });
+
+
+export default addNote(data) {
+    const note = {
+        created: Date.now(),
+        title: data.title,
+        text: data.text,
+        tags: [...data.tags],
+        lastEditTime: Date.now()
+    };
+    const userId = firebase.auth().currentUser.uid;
+    const notesRef = database.ref(`/notes/${userId}`);
+    return notesRef.push(note)
+        .then(res => {
+            note.key = res.key;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+}
+
+var userId = firebase.auth().currentUser.uid;
+return firebase.database().ref('/notes/' + userId).once('value').then(function (snapshot) {
+    return snapshot;
+}).catch(function(error) {
+    console.log(error)
+});
